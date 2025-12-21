1a861398db78b0424a9615da82baaa810e3c6074
BBVALENTIN <babeanugeorgevalentin@gmail.com>
2025-12-21 15:11:15 +0200

    fixed bishop

--- src/main/java/com/sah/game/piese/Nebun.java
+++ src/main/java/com/sah/game/piese/Nebun.java
@@ -11,7 +11,9 @@ public class Nebun  extends Piese {
 
     @Override
     public boolean miscare(int targetRow, int targetCol) {
-        if(patratValid(targetRow, targetCol) && Math.abs(targetRow - this.row) == Math.abs(targetCol - this.col) && piesaPeDiagonala(targetRow, targetCol) == false && acelasiPatrat(targetRow, targetCol))
+        if(acelasiPatrat(targetRow, targetCol))
+            return false;
+        if(patratValid(targetRow, targetCol) && Math.abs(targetRow - this.row) == Math.abs(targetCol - this.col) && piesaPeDiagonala(targetRow, targetCol) == false)
             return true;
         System.out.println("lovestePiese:"+lovestePiese+" piesaPeDiagonala" + this.piesaPeDiagonala(targetRow, targetCol) + " patratvalid: "+ patratValid(targetRow, targetCol));
         return false;
