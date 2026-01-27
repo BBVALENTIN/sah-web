2003c7af9f561b1763107d4dbc298f0e465f6771
BBVALENTIN <babeanugeorgevalentin@gmail.com>
2026-01-27 12:08:35 +0200

    Added basic tables to the database

---
+++ src/main/java/com/sah/entity/Chess_Games_Classic.java
@@ -0,0 +1,37 @@
+package com.sah.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Chess_Games_Classic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ClassicGameId;
+
+    @Column(nullable = false)
+    private int userWhiteId;
+    @Column(nullable = false)
+    private int userBlackId;
+    @Column(nullable = false)
+    private String result;
+    @Column(nullable = false)
+    private int number_of_moves = 0;
+    @Column(nullable = false)
+    private String PGN;
+
+    public Long getClassicGameId() { return ClassicGameId; }
+
+    public int getUserWhiteId() { return userWhiteId; }
+    public void setUserWhiteId(int userWhiteId) { this.userWhiteId = userWhiteId; }
+
+    public int getUserBlackId() { return userBlackId; }
+    public void setUserBlackId(int userBlackId) { this.userBlackId = userBlackId; }
+
+    public String getResult() { return result; }
+    public void setResult() { this.result = result; }
+
+    public String getPGN() { return PGN; }
+    public void setPGN(String PGN) { this.PGN = PGN; }
+
+
+}
