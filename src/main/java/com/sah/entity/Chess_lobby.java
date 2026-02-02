cf961248084ebc666193020e0b47695541ae89da
BBVALENTIN <babeanugeorgevalentin@gmail.com>
2026-02-02 11:19:10 +0200

    first implementation of websocket config
    added lombok to the project, and changed the chess_lobby intentity to fit more of my neeeds. And a small implementation of websockets

--- src/main/java/com/sah/entity/Chess_lobby.java
+++ src/main/java/com/sah/entity/Chess_lobby.java
@@ -8,9 +8,19 @@ public class Chess_lobby {
 
     @Column(nullable = false, name = "format")
     private String format;
+    @Column(nullable = false)
+    private String player_white;
+    @Column(nullable = false)
+    private String player_black;
 
     public String getLobby_Id() { return lobby_Id; }
 
     public String getFormat() { return format; }
     public void setFormat(String format) { this.format = format; }
+
+    public String getPlayer_white() { return player_white; }
+    public void setPlayer_white(String player_white) { this.player_white = player_white; }
+
+    public String getPlayer_black() { return player_black; }
+    public void setPlayer_black(String player_black) { this.player_black = player_black; }
 }
