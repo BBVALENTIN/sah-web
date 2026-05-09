6218730f5e7e47907f1940c7284fe82031e3a8f9
BBVALENTIN <babeanugeorgevalentin@gmail.com>
2026-05-09 22:14:52 +0300

    Added posts tables in db

---
+++ src/main/java/com/sah/entity/PostsComments.java
@@ -0,0 +1,26 @@
+package com.sah.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class PostsComments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Posts post;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    @Column(nullable = false)
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
