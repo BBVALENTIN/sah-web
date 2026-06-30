fec1a47c50080b6bd35bf7d4166c9eed388a9455
BBVALENTIN <babeanugeorgevalentin@gmail.com>
2026-06-30 16:18:45 +0300

    Bug fix, style change, and typo fixed
    Fixed a bug where the lobby count would show an incorrect number of lobbies, style change for the lobby page and typo fixed on the title of login page

--- src/main/resources/static/ts/lobby/GlobalLobby.ts
+++ src/main/resources/static/ts/lobby/GlobalLobby.ts
@@ -21,6 +21,7 @@ function loadLobbies() {
         .then(res => res.json())
         .then((lobbies: lobbyInfo[]) => {
             lobbies.forEach(lobby => addLobbyRow(lobby));
+            updateLobbyCountText();
         })
         .catch(err => console.error("Loading error:", err));
 }
@@ -36,25 +37,28 @@ function connectWS() {
             const lobbyDto: lobbyInfo = JSON.parse(payload.body);
 
             if (lobbyDto.lobbyType === LobbyType.AVAILABLE) {
-                lobbyCount++;
                 addLobbyRow(lobbyDto);
             }
             else {
                 deleteTableRow(lobbyDto.lobbyId);
             }
-            if(lobbyCount === 0) {
-                lobbyInfo.innerText = "There are currently no lobbies open";
-            }
-            else if(lobbyCount === 1){
-                lobbyInfo.innerText = "There is one lobby open";
-            }
-            else {
-                lobbyInfo.innerText = `There are currently ${lobbyCount} lobbies opened`;
-            }
+            updateLobbyCountText();
         });
     }, function(error: any) {
         console.error("STOMP ERROR GLOBAL: ", error);
     });
+}
+
+function updateLobbyCountText() {
+    if(lobbyCount === 0) {
+        lobbyInfo.innerText = "There are currently no lobbies open";
+    }
+    else if(lobbyCount === 1){
+        lobbyInfo.innerText = "There is one lobby open";
+    }
+    else {
+        lobbyInfo.innerText = `There are currently ${lobbyCount} lobbies opened`;
+    }
 }
 
 function addLobbyRow(lobby: lobbyInfo) {
@@ -89,20 +93,19 @@ function addLobbyRow(lobby: lobbyInfo) {
 
     if (createdRow) {
         createdRow.innerHTML = `
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">${creator}</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">${lobby.lobbyId}</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">Waiting for opponent...</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;"><button class="join-btn" data-id="${lobby.lobbyId}">Join</button></td>
+           <td data-label="Creator">${creator}</td>
+            <td data-label="Lobby Id">${lobby.lobbyId}</td>
+            <td data-label="Status">Open</td>
+            <td data-label="Action"><button class="join-btn" data-id="${lobby.lobbyId}">Join</button></td>
         `;
     } else {
         const tr = document.createElement("tr");
         tr.id = `lobby-${lobby.lobbyId}`;
         tr.innerHTML = `
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">${creator}</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">${lobby.lobbyId}</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;">Open</td>
-            <td style="padding: 10px; border-bottom: 1px solid #080710;"><button class="join-btn" data-id="${lobby.lobbyId}">Join</button></td>
-        `;
+           <td data-label="Creator">${creator}</td>
+            <td data-label="Lobby Id">${lobby.lobbyId}</td>
+            <td data-label="Status">Open</td>
+            <td data-label="Action"><button class="join-btn" data-id="${lobby.lobbyId}">Join</button></td>`
         lobbyCount++;
         tableBody.appendChild(tr);
     }
